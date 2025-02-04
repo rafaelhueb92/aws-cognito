709b59cb6396daf390d89fe02d208d04a15a2010
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-04 17:05:26 -0300

    attach cognito

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,6 +7,10 @@ resource "aws_apigatewayv2_stage" "api_stage" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+  default_route_settings {
+    authorization_type = "JWT"
+    authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
+  }
 }
 
 resource "aws_apigatewayv2_authorizer" "cognito" {
@@ -31,7 +35,7 @@ resource "aws_apigatewayv2_route" "lambda_route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /people"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-  
+
   authorization_type = "JWT"
   authorizer_id = aws_apigatewayv2_authorizer.cognito.id
 }
