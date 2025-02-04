d96187b880743609c69969b2e520d05b46718dfb
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-04 17:43:35 -0300

    attach cognito

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,10 +7,6 @@ resource "aws_apigatewayv2_stage" "api_stage" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
-  default_route_settings {
-    authorization_type = "JWT"
-    authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
-  }
 }
 
 resource "aws_apigatewayv2_authorizer" "cognito" {
