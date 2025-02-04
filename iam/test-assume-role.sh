6913649433e7ccea2e39a0d3695a83d461679776
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-04 16:10:47 -0300

    include authorizer

--- iam/test-assume-role.sh
+++
@@ -1,3 +0,0 @@
-aws sts assume-role-with-web-identity \
-  --role-arn arn:aws:iam::$1:role/GitHubActionsRole \
-  --role-session-name TestSession 
--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -31,6 +31,8 @@ resource "aws_apigatewayv2_route" "lambda_route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /people"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  
+  authorization_type = "JWT"
   authorizer_id = aws_apigatewayv2_authorizer.cognito.id
 }
 
