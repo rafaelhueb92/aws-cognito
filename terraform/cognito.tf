19c714b54ae6ca9172f8dc4de18cb1f73c5bfe89
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-04 21:40:51 -0300

    include config and css

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -18,3 +18,22 @@ resource "aws_cognito_user_pool_domain" "this" {
   domain          = "people-auth"
   user_pool_id    = aws_cognito_user_pool.user_pool.id
 }
+
+resource "aws_cognito_user_pool_client" "this" {
+  name                                 = "people-client"
+  user_pool_id                         = aws_cognito_user_pool.user_pool.id
+  generate_secret                      = false
+  explicit_auth_flows                  = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["openid", "email", "profile"]
+  allowed_oauth_flows_user_pool_client = true
+
+  callback_urls = ["http://localhost:3000/callback"]
+  #logout_urls   = ["https://myfrontend.com/logout"]
+}
+
+resource "aws_cognito_ui_customization" "ui_customization" {
+  user_pool_id = aws_cognito_user_pool.my_user_pool.id
+  client_id    = aws_cognito_user_pool_client.my_app_client.id
+  css          = file("custom-style.css")  # Custom CSS file
+}
